a240d34a870b32a245461dfc06f48fb08e433920
junkfood <junkfood@hust.edu.cn>
2021-10-26 10:45:58 +0800

    初步完成基本要求

---
+++ app/src/main/java/com/example/galleryview/SwipeVideoPlayActivity.java
@@ -0,0 +1,14 @@
+package com.example.galleryview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SwipeVideoPlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_video_play);
+    }
+}
